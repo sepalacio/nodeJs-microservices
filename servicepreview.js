04005c6bb3962aa307ebda63e2b45e365fe3c4d7
Sebastian Palacio <sebastianpalacio327@gmail.com>
2017-04-21 18:43:54 -0500

    Preview microservice

--- servicepreview.js
+++ servicepreview.js
@@ -1,13 +1,49 @@
 'use strict'
 
+const fs = require('fs');
+const mime = require('mime-types');
+const path = require('path');
 const {
-  json
+  json,
+  send
 } = require('micro');
+const Filter = require('instagram_js_filter');
+const effects = new Filter();
 
+module.exports = async(req, res) => {
+  const body = await json(req);
+  const {
+    name,
+    filter
+  } = body;
+  const imagePath = path.join(__dirname, 'uploads', name);
+  let data = '';
+  let preview = '';
+  let type = mime.lookup(imagePath);
 
-module.exports = (req, res) => {
-  const body = json(req);
+  try {
+    data = await convert(imagePath, filter);
+    preview = `data: ${type}; base64, ${data.toString('base64')}`;
+  } catch (err) {
+    return send(res, 500, {
+      'error': err.message
+    });
+  }
 
-  return body;
+  return send(res, 200, {
+    'image': imagePath,
+    'data': data
+  });
+}
 
+function convert(file, filter) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        return reject(err);
+      }
+      const result = effects.apply(data, filter, {});
+      resolve(result);
+    })
+  })
 }
